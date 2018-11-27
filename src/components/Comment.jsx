ca2443c299fa01134f22ffdcf202d9a7928a9a2f
Your Name <jofermin@apptio.com>
2018-11-27 10:19:32 -0700

    created components

---
+++ src/components/Comment.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Comment extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default Comment;
